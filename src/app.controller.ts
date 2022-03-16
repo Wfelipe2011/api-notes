61f7bc10aa00737d8b193c3a97a7f7660b551aea
wilson <wfelipe2011@gmail.com>
2022-03-16 09:31:07 -0300

    init project

---
+++ src/app.controller.ts
@@ -0,0 +1,77 @@
+import { Controller, Get, Param, Delete, Post, Body, BadRequestException, NotAcceptableException } from "@nestjs/common";
+import { logger } from "skyot";
+import { AppService } from "./app.service";
+import { User } from "./database/entity/UserEntity";
+import { Repository } from "./database/repository/Repository";
+
+export interface NotesBody {
+  _id?: string;
+  code: string;
+  email: string;
+  date_processed?: Date;
+  date_created?: string;
+  status?: boolean;
+  nota?: any;
+}
+
+const listaAceitas = ['59'];
+
+@Controller()
+export class AppController {
+  repository: Repository;
+  constructor(private readonly appService: AppService) {
+    this.repository = new Repository(User);
+  }
+
+  @Get('history/:email')
+  async getNotas(@Param() params: { email: string }) {
+    return await this.repository.find({ email: params.email });
+  }
+
+  @Get('job')
+  async startJob() {
+    // chamar job
+  }
+
+  @Delete()
+  async delete() {
+    const data = await this.repository.find<NotesBody>();
+    data.forEach(async (body) => {
+      await this.repository.delete(body._id);
+    });
+  }
+
+  @Post()
+  async getHello(
+    @Body()
+    body: NotesBody,
+  ) {
+    if (!body?.code) throw new BadRequestException('Opa!');
+
+    const codeModel = `${body.code[20]}${body.code[21]}`;
+    if (!listaAceitas.includes(codeModel))
+      throw new NotAcceptableException('Não temos suporte para essa nota');
+
+    logger(`${body.email} solicitou um processamento da nota ${body.code}`);
+    this.setInitialBodyValues(body);
+    try {
+      logger('salvando...');
+      await this.repository.save(body);
+      logger('salvo com sucesso.');
+      return 'Sua nota está sendo processada, consulte o status dentro de alguns minutos';
+    } catch (error) {
+      logger('Essa nota ja foi processada');
+      throw new BadRequestException('Essa nota já foi processada');
+    }
+  }
+
+  private setInitialBodyValues(body: NotesBody) {
+    const [created] = new Date()
+      .toLocaleString('en', {
+        timeZone: 'America/Sao_Paulo',
+      })
+      .split(',');
+    body.date_created = created;
+    body.status = false;
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+require('dotenv').config();
+import { NestFactory } from '@nestjs/core';
+import { logger } from 'skyot';
+import { AppModule } from './app.module';
+import { MongoDBConect } from './database/config/mongoConnect';
+
+export const API = 'http://localhost:3000';
+
+async function bootstrap() {
+  await MongoDBConect.startMongo();
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.listen(process.env.PORT, () => {
+    logger(`Servidor rodando na porta: ${process.env.PORT}`);
+  });
+}
+bootstrap();
