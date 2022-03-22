f46c52accedac27fabcbffac239bda80f0921efa
wilson <wfelipe2011@gmail.com>
2022-03-22 16:57:08 -0300

    ajuste de path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
-import { NotesController } from './module/notes.controller';
+import { NotesController } from './module/notes/notes.controller';
 @Module({
   imports: [],
   controllers: [NotesController],
